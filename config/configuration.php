c5e00b0a6b0175798fca7083c33466bd7bd7bec5
unknown <knezevicandrej32@gmail.com>
2022-02-12 13:34:29 +0100

    commit

---
+++ config/configuration.php
@@ -0,0 +1,30 @@
+<?php
+    // Definisanje apsolutne putanje
+    define("ABSOLUTE_PATH",$_SERVER['DOCUMENT_ROOT']);
+
+    // Definisanje fajlova
+    define("ENV_FILE",ABSOLUTE_PATH."/praktikum/config/.env");
+    
+    @ define("DBNAME",env("DBNAME"));
+    @ define("SERVER",env("SERVER"));
+    @ define("USERNAME",env("USERNAME"));
+    @ define("PASSWORD",env("PASSWORD"));
+    
+    
+
+    function env($naziv){
+        $vrednost = "";
+        $otvaranje = fopen(ENV_FILE,"r");
+        $podaci = file(ENV_FILE);
+    
+        foreach($podaci as $key=>$value){
+            $konfig = explode('=',$value);
+            if($konfig[0] == $naziv){
+                $vrednost = trim($konfig[1]);
+            }
+        }
+        return $vrednost;
+    }
+
+    
+?>
---
+++ views/flexible/author.php
@@ -0,0 +1,23 @@
+<?php
+    include "models/log/upisulog.php";
+    pristup_strani();
+?>
+<div class="container-fluid">
+    <div class="container">
+        <div class="row d-flex justify-content-around pb-5" id="padding">
+            <div class="card mb-4 mt-5">
+            <div class="row no-gutters" id="author">
+                <div class="col-md-4">
+                <img src="assets/img/ja.jpg" class="card-img" alt="moja malenkost">
+                </div>
+                <div class="col-md-8">
+                <div class="card-body">
+                    <h5 class="card-title">About</h5>
+                    <p class="card-text">Hello, my name is Andrej, and i love to call myself Web Developer, altough I am not one (YET!). Two years ago I started learning to code, focusing both on front and back-end development. As you can assume, I had the first encounter with programming at ICT College. I have done multiple web projects by myself and realized that I am really passionate about programming. I am 100% sure that I want to build a successful career in IT, because it is fulfilling, and it makes me happy. If you want to know more things about me, feel free to check out <a href="https://portfolioandrejknezevic.netlify.app/"> my portfolio page</a></p>
+                </div>
+                </div>
+            </div>
+            </div>
+        </div>
+    </div>
+</div>
